7c3892ea5b29cf3e286af7a7d1f136f2d23212cf
maciejimielowski <maciej.imielowski@gmail.com>
2017-11-27 22:15:15 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.create(name: "liski", gender: "a", price: 100, filters: "cute", image_url: "czo.jpg, ")
+Product.create(name: "liski2", gender: "a2", price: 1000, filters: "super cute", image_url: "listki.jpg, ")
